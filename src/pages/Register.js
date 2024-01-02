c4e0a28bb84ccf9ebd2f1b12c6ea0dcd55759bd9
OnShubham <shubhamkharche3005@gmail.com>
2024-01-02 12:59:07 +0530

    update a ui a login & register & header

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -4,7 +4,8 @@ import { useAuth } from "../auth";
 import { Loader } from "../components/loader/loader";
 import md5 from "md5-hash";
 import { Link } from "react-router-dom";
-
+import Background from "../components/img/bg.png";
+import "../components/menu/menu.css";
 // Regular expression for email validation
 const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
@@ -79,101 +80,126 @@ export default function Register() {
   }
 
   return (
-    <div className="content max-w-lg m-auto">
-      <h1 className="text-2xl font-bold text-[#002D74]">
-        Register New Account
-      </h1>
-      {loading && <Loader />}
+    <div className="register" style={{ backgroundImage: `url(${Background})` }}>
+      <div className="register-form  shadow-xl  content max-w-lg m-auto ">
+        <div className="register-form-1">
+          <h1
+            style={{ fontFamily: "Poppins", fontSize: "36px" }}
+            className="text-2xl  font-bold text-[#030E32]"
+          >
+            Register New Account
+          </h1>
+          <h5
+            style={{
+              fontFamily: "Poppins",
+              fontSize: "15px",
+              letterSpacing: "0.15px",
+            }}
+            className="text-[#7E7E7E]"
+          >
+            Enter your email and password with special character
+          </h5>
+          {loading && <Loader />}
 
-      {showForm && (
-        <form onSubmit={postData}>
-          <input
-            type="text"
-            value={formPayload.name}
-            placeholder="Name"
-            onChange={(e) =>
-              setFormPayload({ ...formPayload, name: e.target.value })
-            }
-          />
+          {showForm && (
+            <form onSubmit={postData}>
+              <input
+                className="register-input"
+                type="text"
+                value={formPayload.name}
+                placeholder="Name"
+                onChange={(e) =>
+                  setFormPayload({ ...formPayload, name: e.target.value })
+                }
+              />
 
-          <input
-            type="email"
-            value={formPayload.email}
-            placeholder="Email"
-            onChange={handleEmailChange}
-            className={!emailValid ? "border-red-500" : ""}
-          />
-          {!emailValid && (
-            <p className="text-red-500">This email is incorrect.</p>
+              <input
+                type="email"
+                value={formPayload.email}
+                placeholder="Email"
+                onChange={handleEmailChange}
+                className={!emailValid ? "border-red-500" : "register-input"}
+              />
+              {!emailValid && (
+                <p className="text-red-500">This email is incorrect.</p>
+              )}
+
+              <input
+                type="password"
+                placeholder="Password"
+                className="register-input"
+                onChange={(e) =>
+                  setFormPayload({
+                    ...formPayload,
+                    password: md5(e.target.value),
+                  })
+                }
+              />
+
+              <input
+                type="password"
+                className="register-input"
+                placeholder="Re-enter Password"
+                onChange={(e) =>
+                  setFormPayload({
+                    ...formPayload,
+                    repeat_pass: md5(e.target.value),
+                  })
+                }
+              />
+
+              <button
+                type="submit"
+                style={{ backgroundColor: "#12BF80", color: "white" }}
+                className="button-register shadow-md "
+              >
+                Submit
+              </button>
+            </form>
           )}
+        </div>
+        {response && response[0].isvalid && (
+          <div>
+            <b className="text-xl text-grey-700">You have been Signed up</b>
+            <Link to="/login">
+              <button className="border-2 bg-white flex text-sm border-blue-600 text-blue-600 hover:hover:text-blue-900 rounded-2xl">
+                Login then
+              </button>
+            </Link>
+          </div>
+        )}
 
-          <input
-            type="password"
-            placeholder="Password"
-            onChange={(e) =>
-              setFormPayload({ ...formPayload, password: md5(e.target.value) })
-            }
-          />
+        {response && !response[0].isvalid && (
+          <div>
+            <b>Error signing up</b>
+            <p className="block">Registration failed. Please try again.</p>
+          </div>
+        )}
 
-          <input
-            type="password"
-            placeholder="Re-enter Password"
-            onChange={(e) =>
-              setFormPayload({
-                ...formPayload,
-                repeat_pass: md5(e.target.value),
-              })
-            }
-          />
+        {badRequest && (
+          <div class="error">
+            <b>{badRequest.httpCode} error</b>
+            {badRequest.httpCode === "400" && (
+              <p>API-endpoint is not configured properly.</p>
+            )}
+            {badRequest.httpCode === "403" && (
+              <p>You have to be logged in to submit this form.</p>
+            )}
+            <p>
+              <code>{badRequest.msg}</code>
+            </p>
+          </div>
+        )}
 
+        {!showForm && !loading && (
           <button
-            type="submit"
-            className="text-white bg-blue-600 hover:hover:text-blue-900 rounded-2xl">
-            Submit
+            className="text-blue-600 border-0 text-sm flex"
+            onClick={() => setShowForm(true)}
+          >
+            Edit my data
           </button>
-        </form>
-      )}
-
-      {response && response[0].isvalid && (
-        <div>
-          <b className="text-xl text-grey-700">You have been Signed up</b>
-          <Link to="/login">
-            <button className="border-2 bg-white flex text-sm border-blue-600 text-blue-600 hover:hover:text-blue-900 rounded-2xl">
-              Login then
-            </button>
-          </Link>
-        </div>
-      )}
-
-      {response && !response[0].isvalid && (
-        <div>
-          <b>Error signing up</b>
-          <p className="block">Registration failed. Please try again.</p>
-        </div>
-      )}
-
-      {badRequest && (
-        <div class="error">
-          <b>{badRequest.httpCode} error</b>
-          {badRequest.httpCode === "400" && (
-            <p>API-endpoint is not configured properly.</p>
-          )}
-          {badRequest.httpCode === "403" && (
-            <p>You have to be logged in to submit this form.</p>
-          )}
-          <p>
-            <code>{badRequest.msg}</code>
-          </p>
-        </div>
-      )}
-
-      {!showForm && !loading && (
-        <button
-          className="text-blue-600 border-0 text-sm flex"
-          onClick={() => setShowForm(true)}>
-          Edit my data
-        </button>
-      )}
+        )}
+      </div>
     </div>
   );
 }
